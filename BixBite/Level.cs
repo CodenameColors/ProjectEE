5c13628602daf6df3a94a06a2730e186822d8a80
CodenameColors <calling911x@gmail.com>
2019-04-17 06:36:54 -0400

    Created Sprite layer, and level classes. -Created the sprite layer class, and added a couple methods to help  when the level editor needs to create/interfact with these layers  -Level class was created. The methods there are templated for the   engine interfacing. Import, export, and creating of layers.

---
+++ BixBite/Level.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BixBite.Rendering;
+
+namespace BixBite
+{
+	public class Level
+	{
+		//instance variables
+		public String LevelName = "";
+		public List<SpriteLayer> layers = new List<SpriteLayer>();
+		public Property Properties = new Property();
+
+		public Level(String desname)
+		{
+			LevelName = desname;
+		}
+		public void AddLayer(String LayerName, LayerType deslayertype)
+		{
+			layers.Add(new SpriteLayer(deslayertype) { LayerName = LayerName });
+		}
+
+		public void ImportLevel()
+		{
+
+		}
+
+		public void ExportLevel()
+		{
+
+		}
+
+	}
+}
--- BixBite/Rendering/SpriteLayer.cs
+++ BixBite/Rendering/SpriteLayer.cs
@@ -3,10 +3,80 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BixBite.SceneObject;
+using BixBite.Effects;
 
 namespace BixBite.Rendering
 {
-	class SpriteLayer
+	//This allows this class to have multiple different types/applications.
+	public enum LayerType
 	{
+		None,
+		Tile,			//used for background/tile art
+		Sprite,		//used for sprites, NOT tile
+		Gameobject		//used for location to indicate script triggers.
+	};
+
+	public class SpriteLayer
+	{
+		//instance variables
+		public String LayerName;
+		public LayerType layerType = LayerType.None;
+		List<object> LayerObjects = new List<object>(); //contains the objects for this layer. 
+		ImageEffect layereffect = new ImageEffect();
+
+		public SpriteLayer()
+		{
+		}
+		public SpriteLayer(LayerType desltype)
+		{
+			layerType = desltype;
+		}
+
+
+		/// <summary>
+		/// Add objects to the current sprite layer depending on type.
+		/// </summary>
+		/// <param name="newLayerObject">Desired object to add.</param>
+		public void AddToLayer(object newLayerObject)
+		{
+			switch (layerType)
+			{
+				case (LayerType.None):
+					throw new SpriteLayerException(LayerType.None);
+				case (LayerType.Sprite):
+					if (newLayerObject is Sprite)
+						LayerObjects.Add(newLayerObject);
+					return;
+				case (LayerType.Tile):					
+					return;
+				case (LayerType.Gameobject):
+					if (newLayerObject is Prop || newLayerObject is Spawner || newLayerObject is Transistion)
+						LayerObjects.Add(newLayerObject);
+					return;
+			}
+		}
+
+		/// <summary>
+		/// Handles the drawing of the current layer.
+		/// </summary>
+		public void Draw()
+		{
+
+		}
+
+		class SpriteLayerException : Exception
+		{
+			public SpriteLayerException()
+			{
+
+			}
+
+			public SpriteLayerException(LayerType layerType)
+				: base(String.Format("Invalid Layer Type addtion. Current Layer Type{0}", layerType))
+			{
+
+			}
+		}
 	}
 }
