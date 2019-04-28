57992e96e775467dbd55f7256255cb87f1333203
CodenameColors <calling911x@gmail.com>
2019-04-27 21:21:06 -0400

    Select Tool started
    -Started creating the select tool
    -Select tool allows users to highlight a tile on click
    -Added states for tools. So now only one tool can be used at a time
    -ALSO the state determines what will happen on click.

---
+++ AmethystEngine/Components/Tools/SelectTool.cs
@@ -0,0 +1,91 @@
+﻿using BixBite.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace AmethystEngine.Components.Tools
+{
+	/// <summary>
+	/// This class is here for the select tool.
+	/// The select tool is used in the editor to select objects.
+	/// The selected objects vary on what type of editor is currently in use. 
+	/// </summary>
+	public class SelectTool
+	{
+
+		SpriteLayer Currentlayer;
+		public List<Rectangle> SelectedTiles = new List<Rectangle>();
+
+		/// <summary>
+		/// This method will return ALL the tiles on the level editor tile canvas that are located 
+		/// on a certain layer.
+		/// </summary>
+		public static List<Rectangle> FindTilesOnLayer(Canvas lcanvas, int zindex)
+		{
+			//get all the children
+
+			//seperate all the children based on layers.
+
+			return null;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="layertiles">all the tiles on a given layer</param>
+		/// <param name="x">the x position of the mouse in relative origin to the canvas</param>
+		/// <param name="y">the y position of the mouse in relative origin to the canvas</param>
+		/// <returns>Returns the rectangle that the mouse has clicked on. NULL if not found.</returns>
+		public static Rectangle FindTile(List<Rectangle> layertiles, int zindex, int x, int y)
+		{
+			foreach (Rectangle r in layertiles)
+			{
+				if (inTile(r, x, y))
+				{
+					if (Canvas.GetZIndex(r as System.Windows.UIElement) == zindex)
+						return r;
+				}
+			}
+			return null;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="levelEditorCanvas">the main level editor canvas</param>
+		/// <param name="zindex">The Z index of the layer current selected/desired layer</param>
+		/// <param name="x">the x position of the mouse in relative origin to the canvas</param>
+		/// <param name="y">the y position of the mouse in relative origin to the canvas</param>
+		/// <returns></returns>
+		public static Rectangle FindTile(Canvas levelEditorCanvas, int zindex, int x, int y)
+		{
+
+
+
+			return null;
+		}
+		
+		/// <summary>
+		/// Takes in a tile, and a mouse position. Determines if the mouse is within the tiles bounds.
+		/// </summary>
+		/// <returns>true if the mouse location is within a given range</returns>
+		public static bool inTile(Rectangle tile, int x, int y)
+		{
+			int left = (int)Canvas.GetLeft(tile);
+			int right = left + (int)tile.ActualWidth;
+			int top = (int)Canvas.GetTop(tile);
+			int bottom = top + (int)tile.ActualHeight;
+			if (x >= left && x <= right)
+			{
+				if (y >= top && y <= bottom )
+					return true;
+			}
+			return false;
+		}
+
+	}
+}
