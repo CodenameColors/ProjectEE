1470f967f05dc3c650640609b518e672d1aaa3f2
CodenameColors <calling911x@gmail.com>
2019-05-18 15:48:20 -0400

    Re implemented sprite layers
    -Added the functionailty of sprite layers back.
    -it works on fill, select, eraser, and brush

--- BixBite/Level.cs
+++ BixBite/Level.cs
@@ -20,9 +20,42 @@ namespace BixBite
 			LevelName = desname;
 			Layers = new ObservableCollection<SpriteLayer>();
 		}
+
+		/// <summary>
+		/// This method is here to add a new sprite layer to the current level object
+		/// </summary>
+		/// <param name="LayerName">The name of this layer</param>
+		/// <param name="deslayertype">The type of layer we are adding</param>
 		public void AddLayer(String LayerName, LayerType deslayertype)
 		{
-			Layers.Add(new SpriteLayer(deslayertype) { LayerName = LayerName });
+			int val = 0;
+			for (int i = 0; i < Layers.Count; i++)
+			{
+				if(Layers[i].LayerName == LayerName)
+				{
+					val++;
+					LayerName += val;
+					i = 0; //reset. This will make sure to check ALL layers even  already processed ones.
+				}
+			}
+			Layers.Add(new SpriteLayer(deslayertype, this) { LayerName = LayerName }); //add the layer
+		}
+		
+		/// <summary>
+		/// Finds the SpriteLayer in the current levels layers array. 
+		/// </summary>
+		/// <param name="DesName">The name of the current layer.</param>
+		/// <returns>TRUE: index of the SpriteLayer			FALSE: -1</returns>
+		public int FindLayerindex(String DesName)
+		{
+			for(int i =0; i <Layers.Count; i++)
+			{
+				if(DesName == Layers[i].LayerName)
+				{
+					return i;
+				}
+			}
+			return -1; //failed. The value doesn't exist.
 		}
 
 		public void ImportLevel()
