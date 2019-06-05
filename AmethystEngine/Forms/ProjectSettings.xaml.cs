d29e72697e69c8cebcfb82fe43675d78ee9dd85b
psychoticpolice <calling911x@gmail.com>
2019-06-05 18:08:03 -0400

    The game and the engine are now connected
    -The project settings window will now reflect what the project file displays.
    -The project settings window also lets you change thumbnail, and mainlevel.
    -The MainWindow now has a working thumbanil
    -Started the save project button.
    -added bMainLevel to level class and level file.

--- AmethystEngine/Forms/ProjectSettings.xaml.cs
+++ AmethystEngine/Forms/ProjectSettings.xaml.cs
@@ -21,7 +21,9 @@ namespace AmethystEngine.Forms
   public partial class ProjectSettings : Window
   {
 		String ProjectName, Thumbnail, GameLocation, ConfigLocation = String.Empty;
-		List<Level> levels = new List<Level>();
+		List<String> levelpaths = new List<String>();
+		String ProjectLocation;
+
 
     public ProjectSettings(String ProjectFilepath)
     {
@@ -31,6 +33,7 @@ namespace AmethystEngine.Forms
 
 		private void LoadInitalVars(String FilePath)
 		{
+			ProjectLocation = FilePath;
 			byte[] byteArray = Encoding.UTF8.GetBytes(FilePath);
 
 			using (StreamReader file = new StreamReader(FilePath))
@@ -40,7 +43,7 @@ namespace AmethystEngine.Forms
 
 				while ((ln = file.ReadLine()) != null)
 				{
-					if(ln == "-ProjectName:")
+					if (ln == "-ProjectName:")
 					{
 						ln = file.ReadLine();
 						ProjectName_TB.Text = ln;
@@ -55,13 +58,31 @@ namespace AmethystEngine.Forms
 						ln = file.ReadLine();
 						ConfigLoction_TB.Text = ln;
 					}
-					Console.WriteLine(ln);
+					else if (ln.Contains("Thumbnail")) {
+						ln = file.ReadLine();
+						ProjectThumbnail_IMG.Source = new BitmapImage(new Uri(ln));
+					}
+					else if (ln.Contains("MainLevel"))
+					{
+						ln = file.ReadLine();
+						if (ln.Contains("FILL"))
+							MainLevel_TB.Text = "Not set yet!";
+						else
+							MainLevel_TB.Text = ln.Substring(ln.LastIndexOfAny(new char[] { '/', '\\' }) + 1);
+					}
+						Console.WriteLine(ln);
 					counter++;
 				}
 				file.Close();
 				Console.WriteLine($"File has {counter} lines.");
 			}
-			
+
+			//show levels
+			foreach (String s in Directory.GetFiles(FilePath.Replace(".gem", "_Game\\Content\\Levels")))
+			{
+				Levels_LB.Items.Add(s.Substring(s.LastIndexOfAny(new char[] { '/', '\\' }) + 1));
+				levelpaths.Add(s);
+			}
 		}
 
     private void ProjSettings_DragMove(object sender, MouseButtonEventArgs e)
@@ -69,7 +90,62 @@ namespace AmethystEngine.Forms
       this.DragMove();
     }
 
-    private void ProjSettings_close(object sender, RoutedEventArgs e)
+		private void ChangeThumbnail_BTN_Click(object sender, RoutedEventArgs e)
+		{
+			Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+			dlg.Title = "Choose New Thumbnail";
+			dlg.DefaultExt = "png file (*.png)|*.png"; //default file extension
+			dlg.Filter = "png file (*.png)|*.png";
+
+			// Show save file dialog box
+			Nullable<bool> result = dlg.ShowDialog();
+			// Process save file dialog box results kicks out if the user doesn't select an item.
+			String importfilename, destfilename = "";
+			if (result == true)
+			{
+				importfilename = dlg.FileName;
+			}
+			else
+				return;
+
+			using (StreamReader file = new StreamReader(ProjectLocation))
+			{
+				int counter = 0;
+				string ln;
+				List<String> filelines = new List<string>();
+				while ((ln = file.ReadLine()) != null)
+				{
+					filelines.Add(ln);
+				}
+				file.Close();
+				filelines[3] = importfilename;
+				ProjectThumbnail_IMG.Source = new BitmapImage(new Uri(importfilename));
+				File.WriteAllLines(ProjectLocation, filelines.ToArray());
+			}
+
+		}
+
+		private void SetMainLevel_Click(object sender, RoutedEventArgs e)
+		{
+			Console.WriteLine("Set Main Level");
+			MainLevel_TB.Text = levelpaths[Levels_LB.SelectedIndex].Substring(levelpaths[Levels_LB.SelectedIndex].LastIndexOfAny(new char[] { '/', '\\' }) + 1);
+
+			using (StreamReader file = new StreamReader(ProjectLocation))
+			{
+				int counter = 0;
+				string ln;
+				List<String> filelines = new List<string>();
+				while ((ln = file.ReadLine()) != null)
+				{
+					filelines.Add(ln);
+				}
+				file.Close();
+				filelines[11] = levelpaths[Levels_LB.SelectedIndex];
+				File.WriteAllLines(ProjectLocation, filelines.ToArray());
+			}
+		}
+
+		private void ProjSettings_close(object sender, RoutedEventArgs e)
     {
       this.Close();
     }
