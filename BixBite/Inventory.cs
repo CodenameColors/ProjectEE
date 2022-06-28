525685fcb94c4738c8c49c5e50270440636acc9a
CodenameColors <calling911x@gmail.com>
2022-06-28 01:42:41 -0400

    minor tweaks for DLL continuinty

---
+++ BixBite/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BixBite
+{
+	public class Inventory
+	{
+		public int ID { get; set; }
+		public String Req_Name { get; set; }
+		public String Req_Type { get; set; }
+
+	}
+}
