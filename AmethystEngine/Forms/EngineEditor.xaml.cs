87f7fd95f786b316ea1d4b953f952dbf33a97883
CodenameColors <calling911x@gmail.com>
2023-06-08 00:38:01 -0400

    Before placing the Render points on the screen

--- AmethystEngine/Forms/EngineEditor.xaml.cs
+++ AmethystEngine/Forms/EngineEditor.xaml.cs
@@ -6787,7 +6787,7 @@ namespace AmethystEngine.Forms
 		private Bitmap BitmapImage2Bitmap(BitmapImage bitmapImage)
 		{
 			// BitmapImage bitmapImage = new BitmapImage(new Uri("../Images/test.png", UriKind.Relative));
-
+			 
 			using (MemoryStream outStream = new MemoryStream())
 			{
 				BitmapEncoder enc = new BmpBitmapEncoder();
